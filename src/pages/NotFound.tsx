2fa871990cb20aac29106917355935fe7f29bdc7
shenyaofeng <1051406614@qq.com>
2025-01-17 10:03:08 +0800

    fix:404页面

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,15 +1,16 @@
-import React, { FC } from 'react'
-
-
-
+import { FC } from 'react'
+import { Button, Result } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { MANAGE_INDEX } from '../router';
 const NotFound: FC = () => {
-
+  const navigate = useNavigate();
   return (
-    <>
-      <div >
-        NotFound
-      </div>
-    </>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={<Button type="primary" onClick={() => { navigate(MANAGE_INDEX, { replace: true }) }}>返回问卷列表</Button>}
+    />
   )
 }
 
