68dd3b3bb9e8244d797be2af3e12d772b11ec61d
shenyaofeng <1051406614@qq.com>
2025-01-17 11:00:07 +0800

    星标初始化

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -2,12 +2,14 @@ import React, { FC } from 'react'
 import styles from "./common.module.scss"
 import { useState } from 'react'
 import QuestionCard from '../../components/QuestionCard'
+import { Typography } from 'antd'
 const rawQuestionList = [
   { _id: 'q1', title: '问卷1', isPublished: false, isStar: true, answerCount: 5, createAt: '2022-01-01' },
   { _id: 'q2', title: '问卷2', isPublished: true, isStar: true, answerCount: 3, createAt: '2022-012-01' },
   { _id: 'q3', title: '问卷3', isPublished: false, isStar: false, answerCount: 2, createAt: '2022-013-01' },
-  { _id: 'q4', title: '问卷4', isPublished: false, isStar: false, answerCount: 1, createAt: '2022-014-01' },]
-
+  { _id: 'q4', title: '问卷4', isPublished: false, isStar: false, answerCount: 1, createAt: '2022-014-01' }
+]
+const { Title } = Typography
 
 const List: FC = () => {
   const [questionList, setQuestionList] = useState(rawQuestionList)
@@ -15,7 +17,7 @@ const List: FC = () => {
     <>
       <div className={styles.header}>
         <div className={styles.left}>
-          <h3>我的问卷</h3>
+          <Title level={3}>我的问卷</Title>
         </div>
         <div className={styles.right}>搜索</div>
       </div>
--- src/pages/manage/Star.tsx
+++ src/pages/manage/Star.tsx
@@ -1,13 +1,34 @@
 import React, { FC } from 'react'
-
-
-
+import styles from "./common.module.scss"
+import { useState } from 'react'
+import QuestionCard from '../../components/QuestionCard'
+import { Typography,Empty } from 'antd'
+const rawQuestionList = [
+  { _id: 'q1', title: '问卷1', isPublished: false, isStar: true, answerCount: 5, createAt: '2022-01-01' },
+  { _id: 'q2', title: '问卷2', isPublished: true, isStar: true, answerCount: 3, createAt: '2022-012-01' },
+  { _id: 'q3', title: '问卷3', isPublished: false, isStar: true, answerCount: 2, createAt: '2022-013-01' },
+  { _id: 'q4', title: '问卷4', isPublished: false, isStar: true, answerCount: 1, createAt: '2022-014-01' }
+]
+const { Title } = Typography
 const Star: FC = () => {
-
+const [questionList, setQuestionList] = useState(rawQuestionList)
   return (
     <>
-      <div >
-        Star
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <Title level={3}>我的问卷</Title>
+        </div>
+        <div className={styles.right}>搜索</div>
+      </div>
+      <div className='content' style={{ backgroundColor: '#f1f1f1' }}>
+        {/* 问卷列表 */}
+        {questionList.length === 0 ? <Empty></Empty> : questionList.map((item) => {
+          const { _id } = item
+          return <QuestionCard key={_id} {...item} ></QuestionCard>
+        })} 
+      </div>
+      <div className={styles.footer}>
+        加载更多
       </div>
     </>
   )
