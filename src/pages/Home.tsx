1dd35ff85a625ad714736660dc3266d701e0eaac
shenyaofeng <1051406614@qq.com>
2025-01-17 09:54:19 +0800

    fix:home页

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,21 @@
 import React, { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
-
-
+import { Button, Typography } from 'antd'
+import { MANAGE_INDEX } from '../router'
+import styles from './Home.module.scss'
+const {Title,Paragraph} = Typography
 const Home: FC = () => {
   const navigate = useNavigate()
   return (
     <>
-      <div >
-        Home
+      <div className={styles.continer}>
+        <div className={styles.info}>
+          <Title>问卷调查 | 在线投票</Title>
+          <Paragraph>已累计13123123五分,发布12312,收到23123分</Paragraph>
+          <div>
+            <Button type='primary' onClick={() => { navigate(MANAGE_INDEX) }}>开始使用</Button>
+          </div>
+        </div>
       </div>
     </>
   )
